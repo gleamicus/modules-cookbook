0f05623474d6eb67540f097532c5a6ebbe81d5a7
Ed Fisher <ed@mrskin.com>
2014-07-24 15:58:25 -0500

    fix for newer ubuntu versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@ default['modules']['packages'] = value_for_platform_family(
   'debian' => value_for_platform(
     'ubuntu' => {
       'default' => ['kmod'],
-      ['10.04', '12.04', '12.10'] => ['module-init-tools'],
+      ['10.04', '12.04', '12.10', '13.04', '13.10', '14.04', '14.10'] => ['module-init-tools'],
     },
     'default' => ['kmod']
   ),
